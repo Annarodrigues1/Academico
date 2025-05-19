370232c73a6a9bf9e10c3f188639ad7ebff57ab4
Annarodrigues1 <anabeatrizreserva7@gmail.com>
2025-05-19 10:04:20 -0300

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class PessoaInline(admin.TabularInline):
+    model = Pessoa
+    extra = 1
+
+class OcupacaoAdmin(admin.ModelAdmin):
+    inlines = [PessoaInline]
+
+class CursoInlineInstituicao(admin.TabularInline):
+    model = Curso
+    extra = 1
+
+class InstituicaoEnsinoAdmin(admin.ModelAdmin):
+    inlines = [CursoInlineInstituicao]
+
+# Inline 3: Área do Saber e Curso
+class CursoInlineArea(admin.TabularInline):
+    model = Curso
+    extra = 1
+
+class AreaSaberAdmin(admin.ModelAdmin):
+    inlines = [CursoInlineArea]
+
+class CursoDisciplinaInline(admin.TabularInline):
+    model = CursoDisciplina
+    extra = 1
+
+class CursoAdmin(admin.ModelAdmin):
+    inlines = [CursoDisciplinaInline]
+
+class AvaliacaoInline(admin.TabularInline):
+    model = Avaliacao
+    extra = 1
+
+class DisciplinaAdmin(admin.ModelAdmin):
+    inlines = [AvaliacaoInline]
+
+class PessoaTurmaInline(admin.TabularInline):
+    model = PessoaTurma
+    extra = 1
+
+class TurmaAdmin(admin.ModelAdmin):
+    inlines = [PessoaTurmaInline]
+
+class FrequenciaInline(admin.TabularInline):
+    model = Frequencia
+    extra = 1
+
+class PessoaAdmin(admin.ModelAdmin):
+    inlines = [FrequenciaInline]
+
+admin.site.register(Cidade)
+admin.site.register(Ocupacao, OcupacaoAdmin)
+admin.site.register(Pessoa, PessoaAdmin)
+admin.site.register(InstituicaoEnsino, InstituicaoEnsinoAdmin)
+admin.site.register(AreaSaber, AreaSaberAdmin)
+admin.site.register(Curso, CursoAdmin)
+admin.site.register(Turma, TurmaAdmin)
+admin.site.register(Disciplina, DisciplinaAdmin)
+admin.site.register(Matricula)
+admin.site.register(Avaliacao)
+admin.site.register(Frequencia)
+admin.site.register(Turnos)
+admin.site.register(Ocorrencia)
+admin.site.register(CursoDisciplina)
+admin.site.register(AvaliacaoTipo)
+admin.site.register(PessoaTurma)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,97 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from .models import *
 
-# Create your views here.
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+# RF01 - Pessoa
+class PessoasView(View):
+    def get(self, request, *args, **kwargs):
+        pessoas = Pessoa.objects.all()
+        return render(request, 'pessoa.html', {'pessoas': pessoas})
+
+# RF02 - Ocupação
+class OcupacoesView(View):
+    def get(self, request, *args, **kwargs):
+        ocupacoes = Ocupacao.objects.all()
+        return render(request, 'ocupacao.html', {'ocupacoes': ocupacoes})
+
+# RF03 - Instituição
+class InstituicoesView(View):
+    def get(self, request, *args, **kwargs):
+        instituicoes = InstituicaoEnsino.objects.all()
+        return render(request, 'instituicao.html', {'instituicoes': instituicoes})
+
+# RF04 - Área do Saber
+class AreasSaberView(View):
+    def get(self, request, *args, **kwargs):
+        areas = AreaSaber.objects.all()
+        return render(request, 'area_saber.html', {'areas': areas})
+
+# RF05 - Curso
+class CursosView(View):
+    def get(self, request, *args, **kwargs):
+        cursos = Curso.objects.all()
+        return render(request, 'curso.html', {'cursos': cursos})
+
+# RF06 - Turma
+class TurmasView(View):
+    def get(self, request, *args, **kwargs):
+        turmas = Turma.objects.all()
+        return render(request, 'turma.html', {'turmas': turmas})
+
+# RF07 - Disciplina
+class DisciplinasView(View):
+    def get(self, request, *args, **kwargs):
+        disciplinas = Disciplina.objects.all()
+        return render(request, 'disciplina.html', {'disciplinas': disciplinas})
+
+# RF08 - Matrícula
+class MatriculasView(View):
+    def get(self, request, *args, **kwargs):
+        matriculas = Matricula.objects.all()
+        return render(request, 'matricula.html', {'matriculas': matriculas})
+
+# RF09 - Avaliação
+class AvaliacoesView(View):
+    def get(self, request, *args, **kwargs):
+        avaliacoes = Avaliacao.objects.all()
+        return render(request, 'avaliacao.html', {'avaliacoes': avaliacoes})
+
+# RF10 - Frequência
+class FrequenciasView(View):
+    def get(self, request, *args, **kwargs):
+        frequencias = Frequencia.objects.all()
+        return render(request, 'frequencia.html', {'frequencias': frequencias})
+
+# RF11 - Turnos
+class TurnosView(View):
+    def get(self, request, *args, **kwargs):
+        turnos = Turnos.objects.all()
+        return render(request, 'turnos.html', {'turnos': turnos})
+
+# RF12 - Cidade
+class CidadesView(View):
+    def get(self, request, *args, **kwargs):
+        cidades = Cidade.objects.all()
+        return render(request, 'cidade.html', {'cidades': cidades})
+
+# RF13 - Ocorrência
+class OcorrenciasView(View):
+    def get(self, request, *args, **kwargs):
+        ocorrencias = Ocorrencia.objects.all()
+        return render(request, 'ocorrencia.html', {'ocorrencias': ocorrencias})
+
+# RF14 - CursoDisciplina
+class CursoDisciplinasView(View):
+    def get(self, request, *args, **kwargs):
+        curso_disciplinas = CursoDisciplina.objects.all()
+        return render(request, 'curso_disciplina.html', {'curso_disciplinas': curso_disciplinas})
+
+# RF15 - AvaliacaoTipo
+class AvaliacaoTiposView(View):
+    def get(self, request, *args, **kwargs):
+        tipos = AvaliacaoTipo.objects.all()
+        return render(request, 'avaliacao_tipo.html', {'tipos': tipos})
